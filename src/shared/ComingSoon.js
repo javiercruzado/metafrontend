39b9ecf0fa827cfc05053fc9aac887097f8bc399
Javier <jacruzadoc@gmail.com>
2024-09-26 00:33:42 -0500

    feat: new component "Coming Soon"

---
+++ src/shared/ComingSoon.js
@@ -0,0 +1,11 @@
+import Recent from "../assets/Recent.svg";
+
+const ComingSoon = () => {
+  return (
+    <>
+      <h1>Coming Soon!</h1>
+      <img alt="" src={Recent} width={"150px"}></img>
+    </>
+  );
+};
+export default ComingSoon;
