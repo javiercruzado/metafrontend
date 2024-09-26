5e62f6440fecac2c6318753517863f74c4c7365f
Javier <jacruzadoc@gmail.com>
2024-09-26 00:33:06 -0500

    feat: Lading page improvements

--- src/components/main/homepage/HomePage.js
+++ src/components/main/homepage/HomePage.js
@@ -3,20 +3,29 @@ import "./HomePage.css";
 const HomePage = () => {
   return (
     <>
-      <section className="main-section home-page-section">
-        <h1>Little Lemon</h1>
-        <h2>Chicago</h2>
-        <p>
-          We are a family owned <br />
-          Mediterranean restaurant, <br />
-          focused on traditional
-          <br />
-          recipes server with a modern <br />
-          twist
-        </p>
-        <button>Reserve a table</button>
+      <section className="main-section">
+        <article className="home-page-section">
+          <h1>Little Lemon</h1>
+          <h2>Chicago</h2>
+          <p>
+            We are a family owned <br />
+            Mediterranean restaurant, <br />
+            focused on traditional
+            <br />
+            recipes server with a modern <br />
+            twist
+          </p>
+          <button>Reserve a table</button>
+        </article>
+        <article id="hero-image"></article>
       </section>
-      <section id="hero-image"></section>
+      <section className="home-week-specials">
+        <h2 id="h-week-specials">This week specials!</h2>
+        <button id="b-online-menu">Online Menu</button>
+        {/* {meals.map((meal, index) => 
+        <MealCard key={index} meal={meal} />
+      )} */}
+      </section>
     </>
   );
 };
