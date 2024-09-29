9c701c5ac88947443c85d5c55036391084ed467a
Javier <jacruzadoc@gmail.com>
2024-09-29 00:39:09 -0500

    feat: better style for the booking confirmation

--- src/components/main/bookingpage/ConfirmedBooking.js
+++ src/components/main/bookingpage/ConfirmedBooking.js
@@ -1,8 +1,11 @@
+import Dish from "../../../assets/Dish icon.svg";
 const ConfirmedBooking = () => {
   return (
     <>
-      <section className="main-section book-page-section">
-        <h3>The Booking has been confirmed.</h3>
+      <section className="main confirmation-booking">
+        <h3>The Booking has been confirmed!</h3>
+        <p>You will receive an email with all the details.</p>
+        <img alt="" src={Dish} width={"150px"}></img>
       </section>
     </>
   );
